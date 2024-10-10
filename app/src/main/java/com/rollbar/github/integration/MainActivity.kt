d11d87e7c115bb01a86fc0b84e9d874d5f6e41b5
chris <christian.buongarzoni@gmail.com>
2024-10-10 17:29:51 -0300

    feat: Add Buttons to send errors

--- app/src/main/java/com/rollbar/github/integration/MainActivity.kt
+++ app/src/main/java/com/rollbar/github/integration/MainActivity.kt
@@ -4,26 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rollbar.android.Rollbar
 import com.rollbar.github.integration.ui.theme.GithubIntegrationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val rollbar = Rollbar.init(this, BuildConfig.ROLLBAR_API_KEY, BuildConfig.BUILD_TYPE)
         enableEdgeToEdge()
         setContent {
             GithubIntegrationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize().padding(24.dp),
+                        verticalArrangement = Arrangement.Bottom,
+                    ) {
+                        Button(
+                            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+                            onClick = {
+                                rollbar.error("Some error message")
+                            },
+                        ) {
+                            Text("Log an Error")
+                        }
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                throw RuntimeException("An uncaught Exception")
+                            },
+                        ) {
+                            Text("Throw an Exception")
+                        }
+                    }
                 }
             }
         }
